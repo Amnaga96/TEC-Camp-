e5d5f19d46bf5c0fc4ea2f43ec1545bb7ab72106
Amnaga96 <noorgchot@gmail.com>
2019-12-10 17:45:45 +0200

    first commit: laravel

---
+++ app/Http/Controllers/AskController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AskController extends Controller
+{
+    public function ask(){
+        return view()
+
+    }
+}
---
+++ resources/views/ask.blade.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>health care</title>
+<meta name="description" content="">
+<meta name="author" content="">
+
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<title>
+
+</title>
+<style>
+    .zoom {
+        padding: 10px;
+        transition: transform .2s;
+        /* Animation */
+        width: 80px;
+        height: 80px;
+    }
+    
+    .zoom:hover {
+        transform: scale(1.5);
+        /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
+    }
+    
+    .button {
+        background-color: #86e2d5;
+    }
+    
+    .button:hover {
+        background-color: rgb(83, 81, 81);
+    }
+    
+    #footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: black;
+        color: white;
+        text-align: center;
+        height: 50px;
+    }
+</style>
+
+<body>
+
+    <nav style="background-color: #86e2d5;" class="navbar  navbar-light  fixed-top text-white  navbar-expand-lg ">
+        <a style="color: rgb(83, 81, 81);" class="navbar-brand " href="#">HEALTH CARE</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNav" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+        <div id="myNav" class="collapse navbar-collapse">
+
+
+
+            <!--kmfklermfkmes gf-->
+
+
+            <ul style="margin-left: 260px;" class="navbar-nav mr-auto ">
+
+
+                <li class="ml-3 mr-2 nav-item">
+                    <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+                </li>
+
+
+
+                <li class=" ml-3 mr-2 nav-item">
+                    <a class="nav-link" href="#">ABOUT</a>
+                </li>
+
+
+                <li class="ml-3 mr-2 nav-item">
+                    <a class="nav-link" href="#">SERVICES</a>
+                </li>
+
+
+                <li class="ml-3 mr-2 nav-item">
+                    <a class="nav-link" href="#">OURBLOG</a>
+                </li>
+
+
+                <li class="ml-3 mr-2 nav-item">
+                    <a class="nav-link" href="#">CONTACT US</a>
+                </li>
+
+
+                <li class="ml-3 mr-2 nav-item">
+                    <a class="nav-link" href="#">LOG IN</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <br>
+    <br>
+    <div class="container mb-5">
+        <div>
+            <img src="C:\Users\HP\Desktop\projec\Psychiatrist-vs-therapist-whats-the-difference-4.jpg " class="img-fluid " alt="Responsive image ">
+        </div>
+        <!--<h1>ask</h1>-->
+        <div class="text-center shadow-lg p-3 mb-5 bd-with  bg-white rounded mx-auto d-block text-center">
+            <p>contact therapist</p>
+            <br>
+            <p>Our based doctors will review to ensure you are medically suitable for your requested treatment.....</p>
+            <div class="container">
+                <div class="row">
+                    <div class="col-5 mt-5 rounded mx-auto d-block">
+                        <img class="zoom" src="C:\Users\HP\Desktop\projec\Writing-512.png" alt="write">
+                        <img class="zoom" src="C:\Users\HP\Desktop\projec\im2.png" alt="docters">
+                        <img class="zoom" src="C:\Users\HP\Desktop\projec\1.jpg" alt="help you">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container row text-center mb-2 rounded mx-auto d-block">
+            <div class="col-5 rounded mx-auto d-block">
+                <div class="form-group">
+                    <label for="comment">Your Question:</label>
+                    <textarea style="border: 1px solid #86e2d5;" class=" form-control " rows="5 " id="comment "></textarea>
+                </div>
+                <div class="col-6 mx-auto ">
+                    <div class="form-group ">
+                        <label for="usr ">Your Name:</label>
+                        <input style="border: 1px solid #86e2d5; " type="text " class="form-control " id="usr ">
+                    </div>
+                    <label for="doctors ">Choose your doctor</label>
+                    <div>
+                        <select name="doctor " style="border: 1px solid #86e2d5; border-radius: 4px; ">
+                    <option value="... ">Tasnim</option>
+                    <option value="... ">Soha</option>
+                    <option value="... ">Heba</option>
+                    <option value="... ">Asma</option>
+                </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--
+        <div class="bg-white ">
+            <div class="container row text-center mb-2 mr-5 rounded mx-auto d-block ">
+                <h2>
+                    Questionnaire
+                </h2>
+                <div>
+                    <img src="C:\Users\HP\Desktop\projec\Psychiatrist-vs-therapist-whats-the-difference-4.jpg " class="img-fluid " alt="Responsive image ">
+                </div>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        1----
+                    </li>
+                    <li>
+                        2------
+                    </li>
+                    <li>
+                        3-------
+                    </li>
+                </ul>
+            </div>
+            -->
+        <div class="col-6 mt-3 mb-3 ml-5">
+            <button class="btn button rounded float-right">Send</button>
+        </div>
+        <br>
+        <br>
+    </div>
+    <div id="footer">
+        <div class="container">
+            <p>Copyright © health care. <br> 2019 Design by <a style="color: #86e2d5;" href="team" rel="nofollow">OUR
+                TEAM</a>
+            </p>
+        </div>
+    </div>
+    </div>
+    <script src=" https://code.jquery.com/jquery-3.3.1.slim.min.js " integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo " crossorigin="anonymous "></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js " integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1 " crossorigin="anonymous "></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js " integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM " crossorigin="anonymous "></script>
+</body>
+
+</html>
