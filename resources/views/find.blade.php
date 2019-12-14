d831ed7444bb9eda1e767a754c800bac2af6c37d
Soha <sohaaabusaud@gmail.com>
2019-12-14 16:52:26 +0300

    Soha: first step

--- resources/views/find.blade.php
+++ resources/views/find.blade.php
@@ -6,46 +6,35 @@
 @section('content')
               <!--PUT YOUR CONTENT HERE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!-->
              
-              <div id="showcase">                
-                     
+              <div id="showcase">  
+
+<div style="width:600px;" class="container">
                   <div class="showcase-content">
                       <h2>FIND A THIRAPIST!</h2>
                       <p>Here in health care we can help you to find the avilible therapists in the country.</p>
-
-                      <div class="dropdown">
-
-                          <button style="background-color: #86e2d5;" class="btn btn-info " type="button" data-toggle="dropdown">
-                            CHOOSE A PLACE
-                              <span class="caret"></span></button>
-
-                              <ul class="dropdown-menu">
-                                <input class="form-control" id="myInput" type="text" placeholder="Search..">
-                                <li><a href="#">JANZOUR</a></li>
-                                <li><a href="#">SEYAHYA</a></li>
-                                <li><a href="#">HAI AL-ANDALUS</a></li>
-                                
-                              </ul>
-                              <div style="margin-top: 20px;" class="dropdown">
   
-                                  <button style="background-color: #86e2d5;" class="btn btn-info " type="button" data-toggle="dropdown">
-                                    CHOOSE A TYPE
-                                      <span class="caret"></span></button>
-        
-                                      <ul class="dropdown-menu">
-                                        <input class="form-control" id="myInput" type="text" placeholder="Search..">
-                                        <li><a href="#">KIDS</a></li>
-                                        <li><a href="#">TEEN</a></li>
-                                        <li><a href="#">ADULT</a></li>
-                                        
-                                      </ul>
-                                    
+  <form action="">
+  
+                                 <select class="form-control form-control-lg " style="border-color:#86e2d5 ;">
+                                      <option>select a place</option>
+                                      <option >Janzour</option>
+                                      <option >Seyahia</option>
+                                      <option >hai al-andalus</option>
 
-        
-                              </div>
+                                      </select></p>
+                                      <br>
+                                      <select class="form-control form-control-lg" style="border-color: #86e2d5;">
+                                      <option>select a type</option>
+                                      <option>children</option>
+                                      <option >adult</option>
+                                      <option >teenagers</option>
+
+                                      </select></form>
+
+                    
                               <button style="background-color: #86e2d5; margin-top: 20px;" class="btn btn-info " type="button" >FIND!</button>
-
-                      </div>
-                    </div> 
+</div> 
                       
+                    </div>                     
             </div>
 @endsection
