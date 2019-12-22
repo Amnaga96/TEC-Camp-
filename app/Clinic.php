a7484742f9339e696544bb5171dc9bce032193da
Hani Majebry <hani.majebry@gmail.com>
2019-12-22 21:02:55 +0200

    Update therapists and questions features

--- app/Clinic.php
+++ app/Clinic.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Clinic extends Model
 {
-
+    public function therapists()
+    {
+        return $this->belongsToMany('App\User'); // ->where('user_type', 'therapist')
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,12 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('therapists', 'TherapistController@index'); // this is a page to show consulters for users
-// create
-// store
+Route::get('therapists/create', 'TherapistController@create')->middleware('auth-admin');
+Route::post('therapists', 'TherapistController@store');
 
 Route::get('questions', 'QuestionController@index')->middleware('auth'); // getting patient's questions for them
 Route::post('questions/{id}/replies', 'QuestionReplyController@store')->middleware('auth');
+
+// Route::get('clinics', 'ClinicController@index');
+// Route::get('clinics/create', 'ClinicController@create');
+// Route::post('clinics', 'ClinicController@store');
