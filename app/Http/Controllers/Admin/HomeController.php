163af6158f90d74be27e545b78750eeb5ffbb038
Amnaga96 <noorgchot@gmail.com>
2019-12-28 15:49:15 +0200

    Amna

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\User;
 
 class HomeController extends Controller
 {
-    public function index()
-    {
-        return view('admin.home');
-    }
+    public function index($id){
+
+     $user =  user::with('name')->find($id);
+        return view('admin', compact('user'));
+    
+
+}
 }
