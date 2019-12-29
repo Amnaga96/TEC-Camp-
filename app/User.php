7da45d8f6828d68f686cda245aa9e1af078462f0
Amnaga96 <noorgchot@gmail.com>
2019-12-29 16:53:17 +0200

    Amna: add api

--- app/User.php
+++ app/User.php
@@ -6,7 +6,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use  Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject 
 {
     use Notifiable;
 
@@ -14,7 +16,28 @@ class User extends Authenticatable
      * The attributes that are mass assignable.
      *
      * @var array
+     * @return mixed
      */
+        //add  JWT
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+
+
+
+
     protected $fillable = [
         'name', 'email', 'password', 'user_type'
     ];
@@ -51,4 +74,6 @@ class User extends Authenticatable
     {
         return $this->belongsToMany('App\Clinic');
     }
+    
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -1,2 +1,13 @@
 <?php
 
+Route::post('patient/register', 'API\PatientRegisterController@register');
+
+Route::post('/login', 'API\LoginController@login');
+
+
+
+    Route::post('user/auth/login', 'API\user\AuthController@login');
+    Route::post('user/auth/logout', 'API\user\AuthController@logout');//->middleware('auth:api');
+    //Route::post('user/auth/refresh', 'AuthController@refresh');
+    Route::get('user/auth/me', 'API\user\AuthController@me');//->middleware('auth:api');
+   
