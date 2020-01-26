c063180fe2fc7383a76cf27a11ae47b1ca30e4c0
Amnaga96 <noorgchot@gmail.com>
2020-01-26 16:35:02 +0200

    Amna , edit

--- routes/api.php
+++ routes/api.php
@@ -2,20 +2,20 @@
 
 Route::get('/blog', 'API\ArticleController@index');
 Route::get('/article/{a_id}', 'API\ArticleController@show');
-Route::post('patient/register', 'API\PatientRegisterController@register');
+//Route::post('patient/register', 'API\PatientRegisterController@register');
 
 Route::post('/login', 'API\LoginController@login');
 
 
 Route::get('/find-therapist','API\FindTherapistController@index');
-Route::post('patient/register', 'API\PatientRegisterController@register');
+Route::post('patient/register', 'API\PatientRegisterController@create');
 
 // Route::post('/login', 'API\LoginController@login');
 // +
 Route::post('user/auth/login', 'API\user\AuthController@login');
 Route::post('user/auth/logout', 'API\user\AuthController@logout') ->middleware('auth:api');
-Route::post('user/auth/refresh', 'AuthController@refresh');
-Route::get('user/auth/me', 'API\user\AuthController@me');//->middleware('auth:api');
+Route::post('user/auth/refresh', 'API\user\AuthController@refresh');
+Route::get('user/auth/me', 'API\user\AuthController@me')->middleware('auth:api');
    
 Route::get('/questions', 'API\QuestionController@index');
 Route::post('/questions', 'API\QuestionController@store');
