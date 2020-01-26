a50d3edc1dd086acdbd4e56aecb7f5e7fdddee5e
Amnaga96 <noorgchot@gmail.com>
2020-01-26 19:35:39 +0200

    Amna : edit

--- routes/api.php
+++ routes/api.php
@@ -11,12 +11,6 @@
 Route::get('/find-therapist','API\FindTherapistController@index');
 Route::post('patient/register', 'API\PatientRegisterController@create');
 
-<<<<<<< HEAD
-Route::post('user/auth/login', 'API\user\AuthController@login');
-Route::post('user/auth/logout', 'API\user\AuthController@logout') ->middleware('auth:api');
-Route::post('user/auth/refresh', 'API\user\AuthController@refresh');
-Route::get('user/auth/me', 'API\user\AuthController@me');//->middleware('auth:api');
-=======
 // Route::post('/login', 'API\LoginController@login');
 // +
 Route::post('user/auth/login', 'Api\AuthController@login');
@@ -29,6 +23,5 @@ Route::post('/questions', 'Api\QuestionController@store');
 Route::get('/questions/{qid}', 'Api\QuestionController@show');
 
 Route::post('/questions/{qid}/replies', 'Api\QuestionReplyController@store');
->>>>>>> 994f80ce0bc41dfe01f18de163c7b8061663cf95
 
 
