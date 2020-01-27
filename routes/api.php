672023fa14bd9d2399824a56a2f7df390ede69cc
Amnaga96 <noorgchot@gmail.com>
2020-01-27 19:53:59 +0200

    Amna chekout

--- routes/api.php
+++ routes/api.php
@@ -3,20 +3,18 @@
 
 // Route::get('/blog', 'API\ArticleController@index');
 // Route::get('/article/{a_id}', 'API\ArticleController@show');
-// // Route::post('patient/register', 'API\PatientRegisterController@register');
-
 // Route::post('/login', 'API\LoginController@login');
 
 
 Route::get('/find-therapist','API\FindTherapistController@index');
-Route::post('patient/register', 'API\PatientRegisterController@create');
+//Route::post('patient/register', 'API\PatientRegisterController@create');
 
 // Route::post('/login', 'API\LoginController@login');
 // +
 Route::post('user/auth/login', 'Api\AuthController@login');
 Route::post('user/auth/logout', 'Api\AuthController@logout') ->middleware('auth:api');
-// Route::post('user/auth/refresh', 'AuthController@refresh');
-Route::get('user/auth/me', 'Api\AuthController@me');//->middleware('auth:api');
+// Route::post('user/auth/refresh', 'Api\AuthController@refresh');
+Route::get('user/auth/me', 'Api\AuthController@me')->middleware('auth:api');
 
 Route::get('/questions', 'Api\QuestionController@index');  // getting patient's questions for them
 Route::post('/questions', 'Api\QuestionController@store');
